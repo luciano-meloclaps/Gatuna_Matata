1a15d42236cb162b5e0a44c0810adffd874f06d8
Luciano Melo Claps <meloclapsluciano@gmail.com>
2023-09-14 18:19:34 -0300

    subiendo proyecto

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Cards from "./components/cards/Cards";
+import Navbar from "./components/navbar/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Cards />
+    </>
+  );
+}
+export default App;
