686caa2264d9e147cea30ee1bd45f38dec8b40e8
Opienso <ezequiel2001uni@gmail.com>
2023-11-04 23:38:20 -0300

    implemente la fake-api, renderizado condicional segun el formulario es para login o para register, los usuarios se guardan en la database de la fake-api

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -10,6 +10,8 @@ import { useNavigate } from "react-router";
 const Login = ({ setSignedInHandler }) => {
   const [email, setEmail] = useState(""); //los state que toman los valores de los input del form
   const [password, setPassword] = useState(""); //los state que toman los valores de los input del form
+  const [name, setName] = useState("");
+  const [loginToggle, setLoginToggle] = useState(false)
 
   const navigate = useNavigate(); //custom hook que después usamos para redirigir al dashboard :p
 
@@ -23,17 +25,81 @@ const Login = ({ setSignedInHandler }) => {
     setPassword(event.target.value);
   };
 
-  const onClickLogInHandler = (event) => {
+  const onChangeNameHandler = (event) => {
+    setName(event.target.value)
+  }
+
+  const onClickSetLoginToggleHandler = () => {
+    setLoginToggle(!loginToggle)
+  }
+
+  const onClickLogInHandler = (event) => { //le hace falta una forma de mostrarle al usuario que ingreso la información incorrecta, tambien hay que hacer un logout, y tambien hay que hacer un renderizado condicional de si el register es para cliente o para niñera, si es para cliente hay que pedir su direccion, así despues se lo encajamos al turno de la niñera :p
     event.preventDefault();
-    if (email.length === 0 || password.length === 0) {
-      //CAMBIAR VALIDACIONES/
-      alert("Usuario inválido para registrarse");
-      return;
+
+    if (loginToggle) { //si es verdadero, entra en el register
+      if (email.length === 0 || password.length === 0) {
+        //CAMBIAR VALIDACIONES/
+        alert("Usuario inválido para registrarse");
+        return;
+      }
+      else {
+        fetch("http://localhost:8000/register", {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify({
+            id: Math.random(),
+            name: name,
+            email: email,
+            password: password,
+            userType: "sitter"
+          }),
+        })
+          .then((response) => {
+            if (response.ok) return response.json();
+            else {
+              throw new Error("the response has some errors!");
+            }
+          })
+          .then((data) => {
+            setSignedInHandler(true); //funcion que pasamos por prop, cambiamos el valor a true así se puede loggear
+            navigate("/dashboard"); //redirigimos al dashboard
+          })
+          .catch((error) => {
+            console.log(error);
+          })
+      }
+    }
+    else { //si es falso, entra en el login
+      fetch("http://localhost:8000/login", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password
+        })
+      })
+        .then((response) => {
+          if (response.ok) return response.json();
+          else {
+            throw new Error("Error en la solicitud de inicio de sesión");
+          }
+        })
+        .then(data => {
+          const token = data.token;
+
+          setSignedInHandler(true); //funcion que pasamos por prop, cambiamos el valor a true así se puede loggear
+          navigate("/dashboard"); //redirigimos al dashboard
+        })
+        .catch(error => {
+          console.error("Error de inicio de sesión:", error);
+        });
     }
 
-    setSignedInHandler(true); //funcion que pasamos por prop, cambiamos el valor a true así se puede loggear
 
-    navigate("/dashboard"); //redirigimos al dashboard
   };
   return (
     <div className="vh-100 d-flex align-items-center background">
@@ -42,11 +108,19 @@ const Login = ({ setSignedInHandler }) => {
           <h1 class="font-marca text-center color-accent-user p-2">
             Gatuna Matata
           </h1>
-
           <p className="text-center color-secondary-user">
             "miau miau miau miau miau miau"
           </p>
           <hr />
+          {loginToggle && <Form.Group className="my-3" controlId="formBasicName">
+            <Form.Label>Nombre</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Ingrese su nombre"
+              onChange={onChangeNameHandler}
+            />
+          </Form.Group>}
+
           <Form.Group className="my-3" controlId="formBasicEmail">
             <Form.Label>Email</Form.Label>
             <Form.Control
@@ -66,16 +140,16 @@ const Login = ({ setSignedInHandler }) => {
             />
           </Form.Group>
 
-          <Form.Group
-            className="mb-3"
-            controlId="formBasicCheckbox"
-          ></Form.Group>
+
+
           <div className="d-grid gap-2 ">
+            <Button onClick={onClickSetLoginToggleHandler} variant="link">{loginToggle ? "¿Ya tienes una cuenta? Inicia sesión" : "¿No tienes cuenta? Registrate"}</Button>
+            <p>{loginToggle.toString()}</p>
             <Button
               size=" btn bg-secondary-user text-white btn-lg px-5 me-md-2 fw-bold bx-2 border-0"
               onClick={onClickLogInHandler}
             >
-              Inciar sesión
+              {loginToggle ? "Registrarse" : "Iniciar sesión"}
             </Button>
           </div>
         </Form>
