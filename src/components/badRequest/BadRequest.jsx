e81e6c3980deaf2877e47e6b629ace028e5d6dbc
CamiloCarabajal <ultrakin3t@gmail.com>
2023-11-17 02:42:30 -0300

    Intento de deployment

--- src/components/badRequest/BadRequest.jsx
+++ src/components/badRequest/BadRequest.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
-/*import "./BadRequest.css"; */
+/*import "./BadRequest.css";dsd */
 
 const Error400 = () => {
   return (
